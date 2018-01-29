9e940de2d79a4f36664484b08d1a05a8198eb951
robertecke83 <35561752+robertecke83@users.noreply.github.com>
2018-01-29 14:34:18 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def sparkhook():
                 botAnswered = api.messages.create(roomId=sparkMsgRoomId, markdown=textAnswer)
 
             elif "help" in sparkMsgText: #Replies to the Message help
-                textAnswer = 'Hello <@personEmail:' + str(jsonAnswer['data']['personEmail']) + '>, I will Help you to Invite People to a Spark Space or Team: \n - In order to invite people into prepare a *.csv and send to me via @mention into the space you want the people added \n - If you want me to add people to a Team I need to be **Moderator** in that specific general Team Space \n - Attached you find a example *.csv \n make sure people in the *.csv are **not** already in the space\team \n - I will **only** work for MeetingZone Employees \n - If you want to know more about me @mention me with **about**'
+                textAnswer = 'Hello <@personEmail:' + str(jsonAnswer['data']['personEmail']) + '>, I will Help you to Invite People to a Spark Space or Team: \n - In order to invite people into prepare a *.csv and send to me via @mention into the space you want the people added \n - If you want me to add people to a Team I need to be **Moderator** in that specific general Team Space \n - Attached you find a example *.csv make sure people in the *.csv are **not** already in the space\team \n - I will **only** work for MeetingZone Employees \n - If you want to know more about me @mention me with **about**'
                 botAnswered = api.messages.create(roomId=sparkMsgRoomId, markdown=textAnswer, files=["https://raw.githubusercontent.com/robertecke83/invitebot2/master/invite.csv"])
 
             else:
@@ -58,7 +58,7 @@ def sparkhook():
                         sparkMsgFileUrl = str(sparkMessage.files[0]) # Get the URL of the first file
                         
                         sparkHeader = {'Authorization': "Bearer " + BOT_TOKEN}
-                        i = 0 # Index to skip title row in the CSV file
+                        i = 0 # Index to skip first row in the CSV file
 
                         with requests.Session() as s: # Creating a session to allow several HTTP messages with one TCP connection
                             getResponse = s.get(sparkMsgFileUrl, headers=sparkHeader) # Get file
